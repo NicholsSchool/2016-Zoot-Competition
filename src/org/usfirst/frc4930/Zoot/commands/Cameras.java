6531f486b64f0d4f51c0fd5abe490e6add04fdc8
Thomas Kubiniec <Koobz@thomass-mbp.nicholsschool.org>
2016-02-13 14:35:49 -0500

    Working Camera Code. Doesn't Switch Orientation

---
+++ src/org/usfirst/frc4930/Zoot/commands/Cameras.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc4930.Zoot.commands;
+
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc4930.Zoot.OI;
+import org.usfirst.frc4930.Zoot.Robot;
+
+public class Cameras {
+	
+	static boolean _cam0 = true; 
+	
+	static public int frontCam;
+	static public int backCam;
+	private static int curCam;
+	private Image frame;
+	private CameraServer server;
+	
+	public Cameras() {
+		frontCam = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        backCam = NIVision.IMAQdxOpenCamera("cam1", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        curCam = frontCam;
+        
+        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+        server = CameraServer.getInstance();
+        server.setQuality(50);
+
+	}
+	
+	public static void changeCam(int newId) {
+
+		NIVision.IMAQdxStopAcquisition(curCam);
+		curCam = newId;
+		NIVision.IMAQdxConfigureGrab(newId);
+    	NIVision.IMAQdxStartAcquisition(newId);
+    	
+    }
+	
+	public void updateCam() {
+    	NIVision.IMAQdxGrab(curCam, frame, 1);
+        server.setImage(frame);
+    }
+	
+	public void initialize() {
+		NIVision.IMAQdxStartAcquisition(curCam);
+		changeCam(frontCam);
+	}
+	public void execute() {
+		
+//		if(Robot.button3IsPressed){
+//			changeCam(frontCam);
+//		
+//		}
+//		if(Robot.button4IsPressed){
+//			changeCam(backCam);
+//		}
+		updateCam();
+	}
+
+
+
+
+
+	
+}
