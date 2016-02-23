d9ee9f6dd078d71665825f1013f48d257a2b258c
CoffeeMusicCode <jon.kiddy@gmail.com>
2016-02-22 19:10:59 -0500

    lets see what happens

--- src/org/usfirst/frc4930/Zoot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc4930/Zoot/subsystems/Pneumatics.java
@@ -2,6 +2,7 @@ package org.usfirst.frc4930.Zoot.subsystems;
 
 import org.usfirst.frc4930.Zoot.RobotMap;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,10 +15,14 @@ public class Pneumatics extends Subsystem {
 
 	public void enableHighGear() {
 		RobotMap.doubleSolenoid.set(DoubleSolenoid.Value.kForward);
+		Timer.delay(2.0);
+		RobotMap.doubleSolenoid.set(DoubleSolenoid.Value.kOff);
 	}
 
 	public void enableLowGear() {
 		RobotMap.doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+		Timer.delay(2.0);
+		RobotMap.doubleSolenoid.set(DoubleSolenoid.Value.kOff);
 	}
 
 	public void startCompressing() {
