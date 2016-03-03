20f7b69f6d7b98302e42b90718577fe55c47ceb3
sree2000 <sreeramjupudy1@gmail.com>
2016-03-03 15:54:05 -0500

    changed arm code

--- src/org/usfirst/frc4930/Zoot/subsystems/ArmLifter.java
+++ src/org/usfirst/frc4930/Zoot/subsystems/ArmLifter.java
@@ -10,18 +10,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class ArmLifter extends Subsystem {
 	
-	private final double HIGH_POT_EXTREME = 1.0;
-	private final double LOW_POT_EXTREME = 0.0;
-	
 	public void initDefaultCommand() {
 		setDefaultCommand(new MoveArm());
 	}
 
 	public void move(double yAxis) {
-		double armPotVal = RobotMap.armPot.get();
-		if ((armPotVal >= LOW_POT_EXTREME) && (armPotVal <= HIGH_POT_EXTREME)) {
-			RobotMap.armDart.set(yAxis);
-		}		
+		RobotMap.armDart.set(yAxis);
 	}
 
 	public void stop() {
