cde38985f82135429dfe1812bea9883646bfe175
sree2000 <sreeramjupudy1@gmail.com>
2016-03-03 16:05:01 -0500

    added gyro code

--- src/org/usfirst/frc4930/Zoot/Robot.java
+++ src/org/usfirst/frc4930/Zoot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4930.Zoot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -18,6 +19,8 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 	public static boolean zoot;
 	public static boolean orientation;
+	public static double gyroAngle;
+	public static double Kp = 0.03;
 
 	// commands
 	public static Command autonomous;
@@ -67,10 +70,16 @@ public class Robot extends IterativeRobot {
 		if (autonomous != null) {
 			autonomous.start();
 		}
+		RobotMap.robotGyro.reset();
 	}
 
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		while (isAutonomous() && isEnabled()) {
+			gyroAngle = RobotMap.robotGyro.getAngle();
+			RobotMap.driveTrainMasterMotors.drive(0.5, gyroAngle * Kp);
+			Timer.delay(0.05);
+		}
 	}
 
 	public void teleopInit() {
