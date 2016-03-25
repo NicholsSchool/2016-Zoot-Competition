0cfd4e2d7bfbd149bc90136538bed7a75c2d4e4d
sree2000 <sreeramjupudy1@gmail.com>
2016-03-25 16:13:45 -0400

    made some auto changes

--- src/org/usfirst/frc4930/Zoot/commands/Autonomous2.java
+++ src/org/usfirst/frc4930/Zoot/commands/Autonomous2.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc4930.Zoot.Robot;
 import org.usfirst.frc4930.Zoot.RobotMap;
-import org.usfirst.frc4930.Zoot.commands.Shoot;
-
 /**
  * Autonomous - lasts 15 seconds
  */
