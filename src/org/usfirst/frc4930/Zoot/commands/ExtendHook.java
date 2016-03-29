c1a542270663a1003b715d783b334ac959d837a5
Jon Kiddy <jon.kiddy@gmail.com>
2016-03-29 14:02:18 -0400

    removed unneeded imports

--- src/org/usfirst/frc4930/Zoot/commands/ExtendHook.java
+++ src/org/usfirst/frc4930/Zoot/commands/ExtendHook.java
@@ -2,7 +2,6 @@ package org.usfirst.frc4930.Zoot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc4930.Zoot.Robot;
-import org.usfirst.frc4930.Zoot.RobotMap;
 
 /**
  * ExtendHook - controls the length of the hook arm
