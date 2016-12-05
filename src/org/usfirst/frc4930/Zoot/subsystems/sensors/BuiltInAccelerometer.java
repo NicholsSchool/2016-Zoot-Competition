104af2387068103da0af31cf1ac1d3d68dc2985e
Jacob Dolan <JacobDolan@jacobs-air.nicholsschool.org>
2016-12-05 16:25:58 -0500

    Initialized jagged aarray for accelerometer history

--- src/org/usfirst/frc4930/Zoot/subsystems/sensors/BuiltInAccelerometer.java
+++ src/org/usfirst/frc4930/Zoot/subsystems/sensors/BuiltInAccelerometer.java
@@ -12,8 +12,6 @@ import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 public class BuiltInAccelerometer extends Command {
 
 	public static BuiltInAccelerometer accelerometer;
-	
-	public double[][] accelHistory = new double[3][10];
 
     public BuiltInAccelerometer() {
     	accelerometer = new BuiltInAccelerometer();
@@ -31,16 +29,17 @@ public class BuiltInAccelerometer extends Command {
     	return accelerometer.getZ();
     }
     
-    public double[][] history() {
+    public double[][] accelHistory = new double[3][10];
+    
+    public void history() {
     	
     	while((int)(Timer.get() *100) % 50 == 0 ){
     		
-    		for(int i = 0; i < accelHistory[0].length;  i++) {
+    		for(int i = 0; i <= 10;  i++) {
     			accelHistory[0][i] = this.xValue();
     			accelHistory[1][i] = this.yValue();
     			accelHistory[2][i] = this.zValue();
     		}
-    		
     		
     	}
     }
